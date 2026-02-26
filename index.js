1c7c4b661d04e4148f2e36f84dc089a42359af8a
domenico-automate <n8nexpert1@gmail.com>
2026-02-26 15:26:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const http = require("http");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Form bot is alive");
 });
 
-server.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port " + PORT);
-});
+server.listen(PORT, "0.0.0.0");
